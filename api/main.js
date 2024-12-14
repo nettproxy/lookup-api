fcdce8d34a9ca5a56fd32706998de4b6c7f15c15
monokaiidev <dingelfelix@gmail.com>
2024-12-14 13:27:20 +0100

    feat: change some stuff

--- api/main.js
+++ api/main.js
@@ -13,55 +13,56 @@ app.get("/", (req, res) => {
 });
 
 
-app.get("/api/v1/user/:id/", function(req,res){
+app.get("/api/v1/user/:id/", async function(req,res){
     let id = req.params.id;
 
     try {
-        fetch(`https://canary.discord.com/api/v10/users/${id}`, {
+        const response = await fetch(`https://canary.discord.com/api/v10/users/${id}`, {
             headers: {
                 "Content-Type": "application/json",
                 Authorization: `Bot ${token.token}`,
             },
-        })
-        .then((res) => res.json())
-        .then((json) => {
-            if (json.message) return res.send(json);
+        });
 
-            const availableBannerFileTypes = [".png",".jpg",".gif"]
+        const json = await response.json();
+        if (json.message) return res.status(404).send(json);
 
-            let bannerUrl = null;
-            let avatarUrl = null;
+        const availableBannerFileTypes = [".png",".jpg",".gif"]
 
-            if (json.banner) {
-                bannerUrl = `https://cdn.discordapp.com/banners/${json.id}/${json.banner}.png?size=1024`
-            }
+        let bannerUrl = null;
+        let avatarUrl = null;
 
-            if (json.avatar) {
-                avatarUrl = `https://cdn.discordapp.com/avatars/${json.id}/${json.avatar}.png?size=1024`
-            }
+        if (json.banner) {
+            bannerUrl = `https://cdn.discordapp.com/banners/${json.id}/${json.banner}.png?size=1024`
+        }
 
-            let pubFlags = [];
+        if (json.avatar) {
+            avatarUrl = `https://cdn.discordapp.com/avatars/${json.id}/${json.avatar}.png?size=1024`
+        }
 
-            FLAGS.forEach((flag) => {
-                if (json.public_flags & flag.bitwise) pubFlags.push(flag.flag)
-            });
+        let flagsFromUser = [];
 
-            console.log(json.public_flags)
+        FLAGS.forEach((flag) => {
+            if (json.public_flags & flag.bitwise) flagsFromUser.push(flag.flag)
+        });
 
-            console.log(pubFlags)
+        // console.log(json.public_flags)
 
-            var createdAt = idToDate(json.id)
+        // console.log(pubFlags)
 
-            let output = {
-               username: json.username,
-               account_created: createdAt.toString().replace("T", "").replace("Z", ""),
-               id: json.id,
-               badges: pubFlags,
-               discriminator: json.discriminator,
-               display_name: json.global_name,
-               nitro: json.premium_type,
-               accent: json.accent_color,
-               avatar: {
+        var createdAt = idToDate(json.id)
+
+        let output = {
+           username: json.username,
+           account_created: createdAt.toString().replace("T", "").replace("Z", ""),
+           id: json.id,
+           badges: pubFlags,
+           discriminator: json.discriminator,
+           display_name: json.global_name,
+           nitro: json.premium_type,
+           accent: json.accent_color,
+           media: {
+            avatar: {
                 avatar: json.avatar,
                 avatar_url: avatarUrl,
                },
@@ -69,77 +70,72 @@ app.get("/api/v1/user/:id/", function(req,res){
                 id: json.banner,
                 url: bannerUrl,
                },
-               clan: {
-                clan_tag: json.clan && json.clan.tag ? json.clan.tag : null,
+           },
+           clan: {
+            clan_tag: json.clan && json.clan.tag ? json.clan.tag : null,
+            guild_id: json.clan && json.clan.identity_guild_id ? json.clan.identity_guild_id : null
+           }
+        }
 
-                guild_id: json.clan && json.clan.identity_guild_id ? json.clan.identity_guild_id : null,
-               }
-            }
-
-            res.send(output);
-        });
+        res.send(output);
     } catch (err) {
-        console.log(err);
+        console.error(err);
+        res.status(500).send({ error: "Internal Server Error" });
     }
 });
-app.get("/api/v1/bot/:id/", function(req,res){
+app.get("/api/v1/bot/:id/", async function(req,res){
     let id = req.params.id;
 
     try {
-        fetch(`https://canary.discord.com/api/v10/applications/${id}/rpc`, {
-        })
-        .then((res) => res.json())
-        .then((json) => {
-            if (json.message) return res.send(json);
+        const response = await fetch(`https://canary.discord.com/api/v10/applications/${id}/rpc`);
+        const json = await response.json();
+        if (json.message) return res.status(404).send(json);
 
 
 
-            let output = {
-                bot_id: json.id,
-                bot_name: json.name,
-                description: json.description,
-                bot_verified: json.is_verified,
-                bot_flags: json.flags,
-                key: json.verify_key,
-                public: json.bot_public
-            }
-            res.send(output);
-        });
+        let output = {
+            bot_id: json.id,
+            bot_name: json.name,
+            description: json.description,
+            bot_verified: json.is_verified,
+            bot_flags: json.flags,
+            key: json.verify_key,
+            public: json.bot_public
+        }
+        res.send(output);
     } catch (err) {
-        console.log(err);
+        console.error(err);
+        res.status(500).send({ error: "Internal Server Error" });
     }
 });
 
-app.get("/api/v1/guild/:id/", function(req, res) {
+app.get("/api/v1/guild/:id/", async function(req, res) {
     let id = req.params.id;
 
     try {
-        fetch(`https://canary.discord.com/api/v10/guilds/${id}/widget.json`, {
+        const response = await fetch(`https://canary.discord.com/api/v10/guilds/${id}/widget.json`, {
             headers: {
                 "Content-Type": "application/json",
             }
-        })
-        .then((res) => res.json())
-        .then((json) => {
-            if (json.code === 50004) {
-                res.status(500);
-                res.send({
-                    error_code: "50004", error: "Guild is non-existent, or not on discovery page!"
-                })
-                return;
-            }
+        });
 
-            let output = {
-                guild_id: json.id,
-                guild_name: json.name,
-                presence_count: json.presence_count,
-                instant_invite: json.instant_invite
-            }
-            res.send(output)
-        })
+        const json = await response.json();
+        if (json.code === 50004) {
+            return res.status(404).send({
+                error_code: "50004", error: "Guild is non-existent, or not on discovery page!"
+            });
+        }
+
+        let output = {
+            guild_id: json.id,
+            guild_name: json.name,
+            presence_count: json.presence_count,
+            instant_invite: json.instant_invite
+        }
+        res.send(output)
     } catch (e) {
-        res.send(e)
-        return res.status(500)
+        console.error(e);
+        res.status(500).send({ error: "Internal Server Error" });
     }
 });
 
